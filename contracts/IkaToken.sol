2b97914f967a44acef9c0c09e1ec158824619020
ibrahimacamara <30411606+ibraheemacamara@users.noreply.github.com>
2020-05-09 11:27:02 +0200

    Update IkaToken.sol
    fix file formating

--- contracts/IkaToken.sol
+++ contracts/IkaToken.sol
@@ -10,11 +10,11 @@ interface ERC20Interface {
 
     function balanceOf(address tokenOwner) external view returns (uint256);
     
-	function allowance(address tokenOwner, address spender) external view returns (uint256);
+    function allowance(address tokenOwner, address spender) external view returns (uint256);
 
     function transfer(address to, uint256 tokens) external returns (bool);
     
-	function approve(address spender, uint256 tokens) external returns (bool);
+    function approve(address spender, uint256 tokens) external returns (bool);
 
     function transferFrom (address from, address to, uint256 tokens) external returns (bool);
     
